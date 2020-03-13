1322f37c740e4daf7eda41f1399d70cc2b0221be
a <a@a.pl>
2020-03-13 07:38:37 +0100

    fresh db

--- ordinner.sql
+++ ordinner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Czas generowania: 06 Lut 2020, 16:23
+-- Czas generowania: 13 Mar 2020, 07:38
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.1
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `food` (
   `ID` int(11) NOT NULL,
-  `FOOD_TYPE` text NOT NULL,
+  `TYPE_ID` int(11) NOT NULL,
   `NAME` text NOT NULL,
   `PRICE` int(11) NOT NULL,
   `image` text NOT NULL
@@ -40,15 +40,15 @@ CREATE TABLE `food` (
 -- Zrzut danych tabeli `food`
 --
 
-INSERT INTO `food` (`ID`, `FOOD_TYPE`, `NAME`, `PRICE`, `image`) VALUES
-(1, '1', 'pomidorowa', 5, 'first.jpg'),
-(2, '1', 'grzybowa', 3, 'second.jpg'),
-(3, '2', 'mielone', 15, 'third.jpg'),
-(4, '2', 'schabowy', 16, 'fourth.jpg'),
-(5, '2', 'kurczak pieczony', 20, 'five.jpg'),
-(6, '3', 'woda', 1, 'six.jpg'),
-(7, '3', 'wino', 10, 'seven.jpg'),
-(8, '3', 'sok', 6, 'eight.jpg');
+INSERT INTO `food` (`ID`, `TYPE_ID`, `NAME`, `PRICE`, `image`) VALUES
+(1, 1, 'pomidorowa', 5, 'first.jpg'),
+(2, 1, 'grzybowa', 3, 'second.jpg'),
+(3, 2, 'mielone', 15, 'third.jpg'),
+(4, 2, 'schabowy', 16, 'fourth.jpg'),
+(5, 2, 'kurczak pieczony', 20, 'five.jpg'),
+(6, 3, 'woda', 1, 'six.jpg'),
+(7, 3, 'wino', 10, 'seven.jpg'),
+(8, 3, 'sok', 6, 'eight.jpg');
 
 -- --------------------------------------------------------
 
@@ -60,36 +60,27 @@ CREATE TABLE `food_order` (
   `ID` int(11) NOT NULL,
   `foodID` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `ORDERID` int(11) NOT NULL
+  `USER_ID` int(11) NOT NULL,
+  `STATUS` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `food_order`
 --
 
-INSERT INTO `food_order` (`ID`, `foodID`, `quantity`, `ORDERID`) VALUES
-(49, 1, 1, 0),
-(50, 4, 1, 0),
-(51, 7, 4, 0),
-(52, 3, 3, 47),
-(53, 7, 1, 47),
-(54, 1, 1, 47),
-(55, 2, 2, 47),
-(56, 1, 3, 47),
-(57, 1, 1, 52),
-(58, 4, 4, 52),
-(59, 4, 4, 52),
-(60, 1, 1, 52),
-(61, 2, 3, 52),
-(62, 4, 1, 52),
-(63, 3, 1, 52),
-(64, 2, 3, 55),
-(65, 2, 1, 56),
-(66, 6, 1, 58),
-(67, 8, 1, 58),
-(68, 4, 1, 58),
-(69, 5, 1, 58),
-(70, 7, 1, 58);
+INSERT INTO `food_order` (`ID`, `foodID`, `quantity`, `USER_ID`, `STATUS`) VALUES
+(71, 1, 1, 2, 'ready'),
+(72, 5, 1, 2, 'ready'),
+(73, 2, 1, 2, 'ready'),
+(74, 1, 3, 2, 'ready'),
+(75, 4, 1, 2, 'ready'),
+(76, 7, 1, 2, 'ready'),
+(77, 2, 2, 2, 'ready'),
+(78, 6, 1, 2, 'ready'),
+(79, 1, 4, 2, 'ready'),
+(80, 6, 1, 2, 'ready'),
+(81, 4, 4, 2, 'ORDERED'),
+(82, 2, 3, 2, 'ready');
 
 -- --------------------------------------------------------
 
@@ -99,7 +90,7 @@ INSERT INTO `food_order` (`ID`, `foodID`, `quantity`, `ORDERID`) VALUES
 
 CREATE TABLE `food_type` (
   `ID` int(11) NOT NULL,
-  `FOOD_NAME` text NOT NULL,
+  `TYPE_NAME` text NOT NULL,
   `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -107,40 +98,10 @@ CREATE TABLE `food_type` (
 -- Zrzut danych tabeli `food_type`
 --
 
-INSERT INTO `food_type` (`ID`, `FOOD_NAME`, `image`) VALUES
+INSERT INTO `food_type` (`ID`, `TYPE_NAME`, `image`) VALUES
 (1, 'soup', 'first.jpg'),
 (2, 'main', 'first.jpg'),
 (3, 'drinks', 'first.jpg');
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `full_order`
---
-
-CREATE TABLE `full_order` (
-  `ID` int(11) NOT NULL,
-  `Date` date NOT NULL,
-  `Status` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Zrzut danych tabeli `full_order`
---
-
-INSERT INTO `full_order` (`ID`, `Date`, `Status`) VALUES
-(47, '2020-02-06', 'ordered'),
-(48, '2020-02-06', 'ordered'),
-(49, '2020-02-06', 'ordered'),
-(50, '2020-02-06', 'ordered'),
-(51, '2020-02-06', 'ordered'),
-(52, '2020-02-06', 'ordered'),
-(53, '2020-02-06', 'ordered'),
-(54, '2020-02-06', 'ordered'),
-(55, '2020-02-06', 'ordered'),
-(56, '2020-02-06', 'ordered'),
-(57, '2020-02-06', 'ordered'),
-(58, '2020-02-06', 'ordered');
 
 -- --------------------------------------------------------
 
@@ -161,8 +122,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`ID`, `Name`, `Role`, `email`, `password`) VALUES
-(1, 'admin', 'admin', 'admin@admin.admin', 'admin'),
-(2, 'user', 'user', 'user@user.user', 'user');
+(1, 'admin', 'admin', 'admin@admin.admin', '$2y$10$dnu49XypLMKeUMjX9KX18.D2Ouf2qTw8/0Zxne4DORvZPWv1I8Ca.'),
+(2, 'user', 'user', 'user@user.user', '$2y$10$IT8ZZN.h.d7BYJIKkDcBwuh5FLHcuu5UPE8g4AxefuAmLwo0nlc1W');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -187,12 +148,6 @@ ALTER TABLE `food_type`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indeksy dla tabeli `full_order`
---
-ALTER TABLE `full_order`
-  ADD PRIMARY KEY (`ID`);
-
---
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
@@ -212,19 +167,13 @@ ALTER TABLE `food`
 -- AUTO_INCREMENT dla tabeli `food_order`
 --
 ALTER TABLE `food_order`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT dla tabeli `food_type`
 --
 ALTER TABLE `food_type`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT dla tabeli `full_order`
---
-ALTER TABLE `full_order`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
